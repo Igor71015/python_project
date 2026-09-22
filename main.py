97cdd9961381b0fb64d29067b778e9e5f62ace0a
Игорь <igorandreev3883@gmail.com>
2026-09-22 10:47:16 +0300

    Финальный коммит: добавлена проверка функций в файле main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from src.widget import get_date, mask_account_card
+
+# Проверка маскировки карт и счетов
+print(mask_account_card("Visa Platinum 7000792289606361"))
+print(mask_account_card("Счет 73654108430135874305"))
+
+# Проверка работы с датой
+print(get_date("2024-03-11T02:26:18.671407"))
