edaae6665106d1a297a395b9d0fd84e731a68de4
Игорь <igorandreev3883@gmail.com>
2026-09-22 10:43:19 +0300

    Первый коммит: добавлен файл .gitignore

---
+++ src/masks.py
@@ -0,0 +1,18 @@
+def get_mask_card_number(card_number: str) -> str:
+
+    card_number = card_number.replace(" ", "")
+
+    if len(card_number) != 16:
+        raise ValueError("Номер карты должен состоять из 16 цифр")
+
+    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}"
+
+
+def get_mask_account(account_number: str) -> str:
+
+    account_number = account_number.replace(" ", "")
+
+    if len(account_number) < 4:
+        raise ValueError("Номер счета должен содержать минимум 4 символа")
+
+    return f"**{account_number[-4:]}"
